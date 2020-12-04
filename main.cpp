54d34bda033d7cbd9f84e8460dde56238db01565
tobias-wilfert <s0181439@ad.ua.ac.be>
2020-12-04 17:12:50 +0100

    Added the basics

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Tobias Wilfert
+//============================================================================
+
+#include <iostream>
+
+/**
+ * This comment is to check that doxygen, works
+ */
+int main() {
+  std::cout << "Hello, World!" << std::endl;
+  return 0;
+}
